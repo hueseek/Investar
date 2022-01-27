810dfd87199cd5c67e0bec6c4cecef7e1567f214
Justin Jak <jjustinys@gmail.com>
2022-01-27 17:41:39 +0900

    생성자, 소멸자 작성

--- DBUpdater.py
+++ DBUpdater.py
@@ -1,12 +1,40 @@
-from typing_extensions import Self
-
+import pymysql
 
 class DBUpdater:
-    def __init__(Self):
+    def __init__(self):
         """생성자: MariaDB 연결 및 종목코드 딕셔너리 생성"""
+        self.conn = pymysql.connect(host='localhost', user='root', passwd='Jays@0802', db='INVESTAR', charset='utf8')
+
+        with self.conn.cursor() as curs:
+            sql = """
+            CREATE TABLE IF NOT EXISTS company_info (
+                code VARCHAR(20),
+                company VARCHAR(20),
+                last_update DATE,
+                PRIMARY KEY (code))
+            """
+            curs.execute(sql)
+            sql = """
+            CREATE TABLE IF NOT EXISTS daily_price (
+                code VARCHAR(20),
+                date DATE,
+                open BIGINT(20),
+                high BIGINT(20),
+                low BIGINT(20),
+                close BIGINT(20),
+                diff BIGINT(20),
+                volume BIGINT(20),
+                PRIMARY KEY (code, date))
+            """
+            curs.execute(sql)
+        self.conn.commit()
+
+        self.codes = dict()
+        self.update_comp_info()    
 
     def __del__(self):
         """소멸자: MariaDB 연결 해제"""
+        self.conn.close()
 
     def read_krx_code(self):
         """KRX로부터 상장법인 목록 파일을 읽어와서 데이터프레임으로 반환"""
